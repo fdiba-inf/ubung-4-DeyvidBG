44961b862da7848852f9ede82809235381561121
Deyvid <>
2021-11-04 06:43:22 +0000

    04.00

---
+++ src/exercise4/MinIndex.java
@@ -0,0 +1,34 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxElement {
+
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int length = input.nextInt();
+    int[] array = new int[length];
+
+    for(int i = 0; i < length; i++) {
+      array[i] = input.nextInt();
+    }
+
+    int index = 0;
+    int min = 99999;
+
+    for(int i = 0; i < array.length; i++) {
+      if(array[i] < min) {
+        min = array[i];
+        index = i;
+      }
+    }
+
+    System.out.println("Min index: " + min);
+
+  }
+}
+
+      
